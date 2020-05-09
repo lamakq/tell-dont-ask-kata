bff0acb90a49c6832aa9621cae17773a8dfad2ce
shujaat.ali <=shujaat.dev@gmail.com>
2020-05-09 15:34:42 +0500

    Product null exception move on Product Catalog

--- src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCase.java
+++ src/main/java/it/gabrieletondi/telldontaskkata/useCase/OrderCreationUseCase.java
@@ -25,19 +25,9 @@ public class OrderCreationUseCase {
     public void run(SellItemsRequest request) {
         Order order = new Order("EUR");
 
-//        order.setStatus(OrderStatus.CREATED);
-//        order.setItems(new ArrayList<>());
-//        order.setCurrency("EUR");
-//        order.setTotal(new BigDecimal("0.00"));
-//        order.setTax(new BigDecimal("0.00"));
-
         for (SellItemRequest itemRequest : request.getRequests()) {
             Product product = productCatalog.getByName(itemRequest.getProductName());
 
-            if (product == null) {
-                throw new UnknownProductException();
-            }
-            else {
                 final BigDecimal unitaryTax = product.getPrice().divide(valueOf(100)).multiply(product.getCategory().getTaxPercentage()).setScale(2, HALF_UP);
                 final BigDecimal unitaryTaxedAmount = product.getPrice().add(unitaryTax).setScale(2, HALF_UP);
                 final BigDecimal taxedAmount = unitaryTaxedAmount.multiply(BigDecimal.valueOf(itemRequest.getQuantity())).setScale(2, HALF_UP);
@@ -52,7 +42,7 @@ public class OrderCreationUseCase {
 
                 order.setTotal(order.getTotal().add(taxedAmount));
                 order.setTax(order.getTax().add(taxAmount));
-            }
+
         }
 
         orderRepository.save(order);
